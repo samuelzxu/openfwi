2cca45c93e592bd4530b26dd455ca239a4667896
Samuel Xu <samuelzxu@gmail.com>
2025-04-21 10:47:58 -0500

    Added a version of the model with BN

--- network.py
+++ network.py
@@ -129,6 +129,69 @@ class ResizeBlock(nn.Module):
         return self.layers(x)
 
 
+# FlatFault/CurveFault
+# 1000, 70 -> 70, 70
+class InversionNetWithBN(nn.Module):
+    def __init__(self, dim1=32, dim2=64, dim3=128, dim4=256, dim5=512, sample_spatial=1.0, **kwargs):
+        super(InversionNetWithBN, self).__init__()
+        self.convblock1 = ConvBlock(5, dim1, kernel_size=(7, 1), stride=(2, 1), padding=(3, 0))
+        self.convblock2_1 = Conv2DwithBN(dim1, dim2, kernel_size=(3, 1), stride=(2, 1), padding=(1, 0))
+        self.convblock2_2 = Conv2DwithBN(dim2, dim2, kernel_size=(3, 1), padding=(1, 0))
+        self.convblock3_1 = Conv2DwithBN(dim2, dim2, kernel_size=(3, 1), stride=(2, 1), padding=(1, 0))
+        self.convblock3_2 = Conv2DwithBN(dim2, dim2, kernel_size=(3, 1), padding=(1, 0))
+        self.convblock4_1 = Conv2DwithBN(dim2, dim3, kernel_size=(3, 1), stride=(2, 1), padding=(1, 0))
+        self.convblock4_2 = Conv2DwithBN(dim3, dim3, kernel_size=(3, 1), padding=(1, 0))
+        self.convblock5_1 = Conv2DwithBN(dim3, dim3, stride=2)
+        self.convblock5_2 = Conv2DwithBN(dim3, dim3)
+        self.convblock6_1 = Conv2DwithBN(dim3, dim4, stride=2)
+        self.convblock6_2 = Conv2DwithBN(dim4, dim4)
+        self.convblock7_1 = Conv2DwithBN(dim4, dim4, stride=2)
+        self.convblock7_2 = Conv2DwithBN(dim4, dim4)
+        self.convblock8 = Conv2DwithBN(dim4, dim5, kernel_size=(8, ceil(70 * sample_spatial / 8)), padding=0)
+        
+        self.deconv1_1 = DeconvBlock(dim5, dim5, kernel_size=5)
+        self.deconv1_2 = Conv2DwithBN(dim5, dim5)
+        self.deconv2_1 = DeconvBlock(dim5, dim4, kernel_size=4, stride=2, padding=1)
+        self.deconv2_2 = Conv2DwithBN(dim4, dim4)
+        self.deconv3_1 = DeconvBlock(dim4, dim3, kernel_size=4, stride=2, padding=1)
+        self.deconv3_2 = Conv2DwithBN(dim3, dim3)
+        self.deconv4_1 = DeconvBlock(dim3, dim2, kernel_size=4, stride=2, padding=1)
+        self.deconv4_2 = Conv2DwithBN(dim2, dim2)
+        self.deconv5_1 = DeconvBlock(dim2, dim1, kernel_size=4, stride=2, padding=1)
+        self.deconv5_2 = Conv2DwithBN(dim1, dim1)
+        self.deconv6 = ConvBlock_Tanh(dim1, 1)
+        
+    def forward(self,x):
+        # Encoder Part
+        x = self.convblock1(x) # (None, 32, 500, 70)
+        x = self.convblock2_1(x) # (None, 64, 250, 70)
+        x = self.convblock2_2(x) # (None, 64, 250, 70)
+        x = self.convblock3_1(x) # (None, 64, 125, 70)
+        x = self.convblock3_2(x) # (None, 64, 125, 70)
+        x = self.convblock4_1(x) # (None, 128, 63, 70) 
+        x = self.convblock4_2(x) # (None, 128, 63, 70)
+        x = self.convblock5_1(x) # (None, 128, 32, 35) 
+        x = self.convblock5_2(x) # (None, 128, 32, 35)
+        x = self.convblock6_1(x) # (None, 256, 16, 18) 
+        x = self.convblock6_2(x) # (None, 256, 16, 18)
+        x = self.convblock7_1(x) # (None, 256, 8, 9) 
+        x = self.convblock7_2(x) # (None, 256, 8, 9)
+        x = self.convblock8(x) # (None, 512, 1, 1)
+        
+        # Decoder Part 
+        x = self.deconv1_1(x) # (None, 512, 5, 5)
+        x = self.deconv1_2(x) # (None, 512, 5, 5)
+        x = self.deconv2_1(x) # (None, 256, 10, 10) 
+        x = self.deconv2_2(x) # (None, 256, 10, 10)
+        x = self.deconv3_1(x) # (None, 128, 20, 20) 
+        x = self.deconv3_2(x) # (None, 128, 20, 20)
+        x = self.deconv4_1(x) # (None, 64, 40, 40) 
+        x = self.deconv4_2(x) # (None, 64, 40, 40)
+        x = self.deconv5_1(x) # (None, 32, 80, 80)
+        x = self.deconv5_2(x) # (None, 32, 80, 80)
+        x = F.pad(x, [-5, -5, -5, -5], mode="constant", value=0) # (None, 32, 70, 70) 125, 100
+        x = self.deconv6(x) # (None, 1, 70, 70)
+        return x
 
 # FlatFault/CurveFault
 # 1000, 70 -> 70, 70
@@ -196,9 +259,9 @@ class InversionNet(nn.Module):
 
 # FlatFault/CurveFault
 # 1000, 70 -> 70, 70
-class InversionNetv2(nn.Module):
+class InversionNetSkip(nn.Module):
     def __init__(self, dim1=32, dim2=64, dim3=128, dim4=256, dim5=512, sample_spatial=1.0, **kwargs):
-        super(InversionNetv2, self).__init__()
+        super(InversionNetSkip, self).__init__()
         self.convblock1 = ConvBlock(5, dim1, kernel_size=(7, 1), stride=(2, 1), padding=(3, 0))
         self.convblock2_1 = ConvBlock(dim1, dim2, kernel_size=(3, 1), stride=(2, 1), padding=(1, 0))
         self.convblock2_2 = ConvBlock(dim2, dim2, kernel_size=(3, 1), padding=(1, 0))
@@ -413,6 +476,7 @@ class Deconv_HPGNN(nn.Module):
 model_dict = {
     'InversionNet': InversionNet,
     'Discriminator': Discriminator,
-    'UPFWI': FCN4_Deep_Resize_2
+    'UPFWI': FCN4_Deep_Resize_2,
+    'InversionNetSkip': InversionNetSkip,
 }
 
