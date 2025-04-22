01d477a554e568a813b54f9c07b03694d7bd220f
Samuel Xu <samuelzxu@gmail.com>
2025-04-22 09:15:46 -0400

    Save

--- train.sh
+++ train.sh
@@ -2,4 +2,12 @@
 # python train.py -ds all -n invnet_all_baseline -m InversionNet -g2v 0 --tensorboard -t train_ds.csv -v val_ds.csv -b 128
 
 # cosine
-python train.py -ds all -n invnet_moredata_cosine -m InversionNet -g2v 0 --tensorboard -t train_ds.csv -v val_ds.csv -b 192
+# python train.py -ds all -n invnet_moredata_cosine -m InversionNet -g2v 0 --tensorboard -t train_ds.csv -v val_ds.csv -b 192
+
+# 
+
+# skip conns
+# python train.py -ds all -n invnet_moredata_cosine -m InversionNetSkip -g2v 0 --tensorboard -t train_ds.csv -v val_ds.csv -b 192
+
+# skip conns
+# python train.py -ds all -n invnet_moredata_cosine -m InversionNetSkip -g2v 0 --tensorboard -t train_ds.csv -v val_ds.csv -b 192
