6d250bc33a788a235ceaf7e2c1df74ae9e40f957
Samuel Xu <samuelzxu@gmail.com>
2025-05-27 16:55:36 -0400

    Add a cool net

--- network.py
+++ network.py
@@ -22,6 +22,12 @@ import torch.nn.functional as F
 from math import ceil
 from collections import OrderedDict
 
+import timm
+from timm.models.convnext import ConvNeXtBlock
+from types import MethodType
+
+from monai.networks.blocks import UpSample, SubpixelUpsample
+
 NORM_LAYERS = { 'bn': nn.BatchNorm2d, 'in': nn.InstanceNorm2d, 'ln': nn.LayerNorm }
 
 # Replace the key names in the checkpoint in which legacy network building blocks are used 
@@ -841,6 +847,458 @@ class UNet(nn.Module):
         # output = logits * 1000.0 + 1500.0
         return logits.squeeze()  # Remove the channel dimension
 
+####################
+## EMA + Ensemble ##
+####################
+
+class ModelEMA(nn.Module):
+    def __init__(self, model, decay=0.99, device=None):
+        super().__init__()
+        self.module = deepcopy(model)
+        self.module.eval()
+        self.decay = decay
+        self.device = device
+        if self.device is not None:
+            self.module.to(device=device)
+
+    def _update(self, model, update_fn):
+        with torch.no_grad():
+            for ema_v, model_v in zip(self.module.state_dict().values(), model.state_dict().values()):
+                if self.device is not None:
+                    model_v = model_v.to(device=self.device)
+                ema_v.copy_(update_fn(ema_v, model_v))
+
+    def update(self, model):
+        self._update(model, update_fn=lambda e, m: self.decay * e + (1. - self.decay) * m)
+
+    def set(self, model):
+        self._update(model, update_fn=lambda e, m: m)
+
+
+class EnsembleModel(nn.Module):
+    def __init__(self, models):
+        super().__init__()
+        self.models = nn.ModuleList(models).eval()
+
+    def forward(self, x):
+        output = None
+        
+        for m in self.models:
+            logits= m(x)
+            
+            if output is None:
+                output = logits
+            else:
+                output += logits
+                
+        output /= len(self.models)
+        return output
+        
+
+#############
+## Decoder ##
+#############
+
+class ConvBnAct2d(nn.Module):
+    def __init__(
+        self,
+        in_channels,
+        out_channels,
+        kernel_size,
+        padding: int = 0,
+        stride: int = 1,
+        norm_layer: nn.Module = nn.Identity,
+        act_layer: nn.Module = nn.ReLU,
+    ):
+        super().__init__()
+
+        self.conv= nn.Conv2d(
+            in_channels, 
+            out_channels,
+            kernel_size,
+            stride=stride, 
+            padding=padding, 
+            bias=False,
+        )
+        self.norm = norm_layer(out_channels) if norm_layer != nn.Identity else nn.Identity()
+        self.act= act_layer(inplace=True)
+
+    def forward(self, x):
+        x = self.conv(x)
+        x = self.norm(x)
+        x = self.act(x)
+        return x
+
+
+class SCSEModule2d(nn.Module):
+    def __init__(self, in_channels, reduction=16):
+        super().__init__()
+        self.cSE = nn.Sequential(
+            nn.AdaptiveAvgPool2d(1),
+            nn.Conv2d(in_channels, in_channels // reduction, 1),
+            nn.Tanh(),
+            nn.Conv2d(in_channels // reduction, in_channels, 1),
+            nn.Sigmoid(),
+        )
+        self.sSE = nn.Sequential(
+            nn.Conv2d(in_channels, 1, 1), 
+            nn.Sigmoid(),
+            )
+
+    def forward(self, x):
+        return x * self.cSE(x) + x * self.sSE(x)
+
+class Attention2d(nn.Module):
+    def __init__(self, name, **params):
+        super().__init__()
+        if name is None:
+            self.attention = nn.Identity(**params)
+        elif name == "scse":
+            self.attention = SCSEModule2d(**params)
+        else:
+            raise ValueError("Attention {} is not implemented".format(name))
+
+    def forward(self, x):
+        return self.attention(x)
+
+class DecoderBlock2d(nn.Module):
+    def __init__(
+        self,
+        in_channels,
+        skip_channels,
+        out_channels,
+        norm_layer: nn.Module = nn.Identity,
+        attention_type: str = None,
+        intermediate_conv: bool = False,
+        upsample_mode: str = "deconv",
+        scale_factor: int = 2,
+    ):
+        super().__init__()
+
+        # Upsample block
+        if upsample_mode == "pixelshuffle":
+            self.upsample= SubpixelUpsample(
+                spatial_dims= 2,
+                in_channels= in_channels,
+                scale_factor= scale_factor,
+            )
+        else:
+            self.upsample = UpSample(
+                spatial_dims= 2,
+                in_channels= in_channels,
+                out_channels= in_channels,
+                scale_factor= scale_factor,
+                mode= upsample_mode,
+            )
+
+        if intermediate_conv:
+            k= 3
+            c= skip_channels if skip_channels != 0 else in_channels
+            self.intermediate_conv = nn.Sequential(
+                ConvBnAct2d(c, c, k, k//2),
+                ConvBnAct2d(c, c, k, k//2),
+                )
+        else:
+            self.intermediate_conv= None
+
+        self.attention1 = Attention2d(
+            name= attention_type, 
+            in_channels= in_channels + skip_channels,
+            )
+
+        self.conv1 = ConvBnAct2d(
+            in_channels + skip_channels,
+            out_channels,
+            kernel_size= 3,
+            padding= 1,
+            norm_layer= norm_layer,
+        )
+
+        self.conv2 = ConvBnAct2d(
+            out_channels,
+            out_channels,
+            kernel_size= 3,
+            padding= 1,
+            norm_layer= norm_layer,
+        )
+        self.attention2 = Attention2d(
+            name= attention_type, 
+            in_channels= out_channels,
+            )
+
+    def forward(self, x, skip=None):
+        x = self.upsample(x)
+
+        if self.intermediate_conv is not None:
+            if skip is not None:
+                skip = self.intermediate_conv(skip)
+            else:
+                x = self.intermediate_conv(x)
+
+        if skip is not None:
+            # print(x.shape, skip.shape)
+            x = torch.cat([x, skip], dim=1)
+            x = self.attention1(x)
+
+        x = self.conv1(x)
+        x = self.conv2(x)
+        x = self.attention2(x)
+        return x
+
+
+class UnetDecoder2d(nn.Module):
+    """
+    Unet decoder.
+    Source: https://arxiv.org/abs/1505.04597
+    """
+    def __init__(
+        self,
+        encoder_channels: tuple[int],
+        skip_channels: tuple[int] = None,
+        decoder_channels: tuple = (256, 128, 64, 32),
+        scale_factors: tuple = (2,2,2,2),
+        norm_layer: nn.Module = nn.Identity,
+        attention_type: str = None,
+        intermediate_conv: bool = False,
+        upsample_mode: str = "deconv",
+    ):
+        super().__init__()
+        
+        if len(encoder_channels) == 4:
+            decoder_channels= decoder_channels[1:]
+        self.decoder_channels= decoder_channels
+        
+        if skip_channels is None:
+            skip_channels= list(encoder_channels[1:]) + [0]
+
+        # Build decoder blocks
+        in_channels= [encoder_channels[0]] + list(decoder_channels[:-1])
+        self.blocks = nn.ModuleList()
+
+        for i, (ic, sc, dc) in enumerate(zip(in_channels, skip_channels, decoder_channels)):
+            # print(i, ic, sc, dc)
+            self.blocks.append(
+                DecoderBlock2d(
+                    ic, sc, dc, 
+                    norm_layer= norm_layer,
+                    attention_type= attention_type,
+                    intermediate_conv= intermediate_conv,
+                    upsample_mode= upsample_mode,
+                    scale_factor= scale_factors[i],
+                    )
+            )
+
+    def forward(self, feats: list[torch.Tensor]):
+        res= [feats[0]]
+        feats= feats[1:]
+
+        # Decoder blocks
+        for i, b in enumerate(self.blocks):
+            skip= feats[i] if i < len(feats) else None
+            res.append(
+                b(res[-1], skip=skip),
+                )
+            
+        return res
+
+class SegmentationHead2d(nn.Module):
+    def __init__(
+        self,
+        in_channels,
+        out_channels,
+        scale_factor: tuple[int] = (2,2),
+        kernel_size: int = 3,
+        mode: str = "nontrainable",
+    ):
+        super().__init__()
+        self.conv= nn.Conv2d(
+            in_channels, out_channels, kernel_size= kernel_size,
+            padding= kernel_size//2
+        )
+        self.upsample = UpSample(
+            spatial_dims= 2,
+            in_channels= out_channels,
+            out_channels= out_channels,
+            scale_factor= scale_factor,
+            mode= mode,
+        )
+
+    def forward(self, x):
+        x = self.conv(x)
+        x = self.upsample(x)
+        return x
+        
+
+#############
+## Encoder ##
+#############
+
+def _convnext_block_forward(self, x):
+    shortcut = x
+    x = self.conv_dw(x)
+
+    if self.use_conv_mlp:
+        x = self.norm(x)
+        x = self.mlp(x)
+    else:
+        x = self.norm(x)
+        x = x.permute(0, 2, 3, 1)
+        x = x.contiguous()
+        x = self.mlp(x)
+        x = x.permute(0, 3, 1, 2)
+        x = x.contiguous()
+
+    if self.gamma is not None:
+        x = x * self.gamma.reshape(1, -1, 1, 1)
+
+    x = self.drop_path(x) + self.shortcut(shortcut)
+    return x
+
+
+class CoolNet(nn.Module):
+    def __init__(
+        self,
+        backbone: str = 'convnext_small.fb_in22k_ft_in1k',
+        pretrained: bool = False,
+    ):
+        super().__init__()
+        
+        # Encoder
+        self.backbone= timm.create_model(
+            backbone,
+            in_chans= 5,
+            pretrained= pretrained,
+            features_only= True,
+            drop_path_rate=0.0,
+            )
+        ecs= [_["num_chs"] for _ in self.backbone.feature_info][::-1]
+
+        # Decoder
+        self.decoder= UnetDecoder2d(
+            encoder_channels= ecs,
+        )
+
+        self.seg_head= SegmentationHead2d(
+            in_channels= self.decoder.decoder_channels[-1],
+            out_channels= 1,
+            scale_factor= 1,
+        )
+        
+        self._update_stem(backbone)
+        
+        self.replace_activations(self.backbone, log=True)
+        self.replace_norms(self.backbone, log=True)
+        self.replace_forwards(self.backbone, log=True)
+
+    def _update_stem(self, backbone):
+        if backbone.startswith("convnext"):
+
+            # Update stride
+            self.backbone.stem_0.stride = (4, 1)
+            self.backbone.stem_0.padding = (0, 2)
+
+            # Duplicate stem layer (to downsample height)
+            with torch.no_grad():
+                w = self.backbone.stem_0.weight
+                new_conv= nn.Conv2d(w.shape[0], w.shape[0], kernel_size=(4, 4), stride=(4, 1), padding=(0, 1))
+                new_conv.weight.copy_(w.repeat(1, (128//w.shape[1])+1, 1, 1)[:, :new_conv.weight.shape[1], :, :])
+                new_conv.bias.copy_(self.backbone.stem_0.bias)
+
+            self.backbone.stem_0= nn.Sequential(
+                nn.ReflectionPad2d((1,1,80,80)),
+                self.backbone.stem_0,
+                new_conv,
+            )
+
+        else:
+            raise ValueError("Custom striding not implemented.")
+        pass
+
+    def replace_activations(self, module, log=False):
+        if log:
+            print(f"Replacing all activations with GELU...")
+        
+        # Apply activations
+        for name, child in module.named_children():
+            if isinstance(child, (
+                nn.ReLU, nn.LeakyReLU, nn.Mish, nn.Sigmoid, 
+                nn.Tanh, nn.Softmax, nn.Hardtanh, nn.ELU, 
+                nn.SELU, nn.PReLU, nn.CELU, nn.GELU, nn.SiLU,
+            )):
+                setattr(module, name, nn.GELU())
+            else:
+                self.replace_activations(child)
+
+    def replace_norms(self, mod, log=False):
+        if log:
+            print(f"Replacing all norms with InstanceNorm...")
+            
+        for name, c in mod.named_children():
+
+            # Get feature size
+            n_feats= None
+            if isinstance(c, (nn.BatchNorm2d, nn.InstanceNorm2d)):
+                n_feats= c.num_features
+            elif isinstance(c, (nn.GroupNorm,)):
+                n_feats= c.num_channels
+            elif isinstance(c, (nn.LayerNorm,)):
+                n_feats= c.normalized_shape[0]
+
+            if n_feats is not None:
+                new = nn.InstanceNorm2d(
+                    n_feats,
+                    affine=True,
+                    )
+                setattr(mod, name, new)
+            else:
+                self.replace_norms(c)
+
+    def replace_forwards(self, mod, log=False):
+        if log:
+            print(f"Replacing forward functions...")
+            
+        for name, c in mod.named_children():
+            if isinstance(c, ConvNeXtBlock):
+                c.forward = MethodType(_convnext_block_forward, c)
+            else:
+                self.replace_forwards(c)
+
+        
+    def proc_flip(self, x_in):
+        x_in= torch.flip(x_in, dims=[-3, -1])
+        x= self.backbone(x_in)
+        x= x[::-1]
+
+        # Decoder
+        x= self.decoder(x)
+        x_seg= self.seg_head(x[-1])
+        x_seg= x_seg[..., 1:-1, 1:-1]
+        x_seg= torch.flip(x_seg, dims=[-1])
+        return x_seg
+
+    def forward(self, batch):
+        x= batch
+
+        # Encoder
+        x_in = x
+        x= self.backbone(x)
+        # print([_.shape for _ in x])
+        x= x[::-1]
+
+        # Decoder
+        x= self.decoder(x)
+        # print([_.shape for _ in x])
+        x_seg= self.seg_head(x[-1])
+        x_seg= x_seg[..., 1:-1, 1:-1]
+        # x_seg= x_seg * 1500 + 3000
+    
+        if self.training:
+            return x_seg
+        else:
+            p1 = self.proc_flip(x_in)
+            x_seg = torch.mean(torch.stack([x_seg, p1]), dim=0)
+            return x_seg
+
 model_dict = {
     'InversionNet': InversionNet,
     'Discriminator': Discriminator,
@@ -848,5 +1306,6 @@ model_dict = {
     'InversionNetSkip': InversionNetSkip,
     'InversionNet3D': InversionNet3D,
     'UNet': UNet,
+    'CoolNet': CoolNet,
 }
 
